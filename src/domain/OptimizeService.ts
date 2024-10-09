bec3ee5064cf9973463d069cc64e5924c48c07d0
Kaustubh Raghavan <Kaustubh@rodeodigital.com>
2024-10-09 14:04:04 +0530

    removed unused api

--- src/domain/OptimizeService.ts
+++ src/domain/OptimizeService.ts
@@ -4,7 +4,6 @@ import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import { IOptimizeService } from './IOptimizeService';
 import axios from 'axios';
-import sharp from 'sharp';
 import { Readable } from 'stream';
 
 
@@ -22,23 +21,23 @@ export class OptimizeService implements IOptimizeService {
 
             imageUrls.map(async (url: string) => {
                 const response = await axios.get(url, { responseType: 'arraybuffer' });
-                const compressedImage = await sharp(response.data).jpeg({ quality: 50 }).toBuffer();
-                const stream = new Readable();
-                stream.push(compressedImage);
-                 stream.push(null);
-                const tempFile: Express.Multer.File = {
-                    fieldname: 'file',
-                    originalname: `sw`,
-                    encoding: '7bit',
-                    mimetype: 'image/jpeg',
-                    buffer: compressedImage,
-                    size: compressedImage.length,
-                    stream: stream,
-                    destination: '',
-                    filename: '',
-                    path: '',
-                  };
-                console.log(tempFile)
+                // const compressedImage = await sharp(response.data).jpeg({ quality: 50 }).toBuffer();
+                // const stream = new Readable();
+                // stream.push(compressedImage);
+                //  stream.push(null);
+                // const tempFile: Express.Multer.File = {
+                //     fieldname: 'file',
+                //     originalname: `sw`,
+                //     encoding: '7bit',
+                //     mimetype: 'image/jpeg',
+                //     buffer: compressedImage,
+                //     size: compressedImage.length,
+                //     stream: stream,
+                //     destination: '',
+                //     filename: '',
+                //     path: '',
+                //   };
+                // console.log(tempFile)
             })
 
         })
